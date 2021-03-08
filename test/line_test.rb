e234145a2e98c4041246c5392a21d4e5b74335c2
kevn7n <kevn_nguyen@yahoo.com>
2021-03-08 04:15:19 -0700

    Add 2 basic line tests

--- test/line_test.rb
+++ test/line_test.rb
@@ -4,9 +4,22 @@ require './lib/line'
 
 class LineTest < Minitest::Test
   def test_reader_exists
-    line = Line.new
+    line = Line.new()
 
     assert_instance_of Line, line
+  end
+
+  def test_has_empty_text_attribute
+    line = Line.new()
+
+    assert_equal "", line.text
+  end
+
+  def test_has_text_attribute
+    text = "abc"
+    line = Line.new(text)
+
+    assert_equal "abc", line.text
   end
 
 end
