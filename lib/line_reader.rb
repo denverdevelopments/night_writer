911e8cd1bf1425f1b8e14b7b3766349a876929a7
kevn7n <kevn_nguyen@yahoo.com>
2021-03-08 03:28:49 -0700

    Add initialize to line_reader

--- lib/line_reader.rb
+++ lib/line_reader.rb
@@ -1,7 +1,25 @@
 class LineReader
+  attr_reader :read_from, :complete
 
-  def initialize
-
+  def initialize(read_from)
+    @complete = []
+    file = File.open(read_from)
+    lines = file.readlines.map(&:chomp)
+    lines.size.times {|n| complete << Line.new(lines[n])}
   end
 
 end
+
+
+# class Generator
+#   attr_reader :cards_file, :cards
+#
+#   def initialize(cards_file)
+#     @cards_file = cards_file
+#     @cards = []
+#     content = File.readlines (@cards_file)
+#     cards_array = content.map {|line| line.split(',')}
+#     cards_array.size.times {|n| cards << Card.new(cards_array[n][0], cards_array[n][1], cards_array[n][2].to_i)}
+#   end
+#
+# end
