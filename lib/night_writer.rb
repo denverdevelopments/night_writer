3382debc9fabe78647f236cf0a08c4fed2429917
kevn7n <kevn_nguyen@yahoo.com>
2021-03-08 03:58:56 -0700

    Add LineReader to runner

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -2,18 +2,23 @@ require './lib/line_reader'
 require './lib/line'
 
 
-# file = File.open("users.txt")
+# print "Please enter the input file to convert to Braille:  "
+# read_from = gets.chomp
 
-file_data = file.read
-# "user1\nuser2\nuser3\n"
+# all_lines = LineReader.new(read_from).complete
+# all_lines = LineGenerator.new('lib/messages.txt').complete
 
-file_data = file.readlines.map(&:chomp)
-# ["user1", "user2", "user3"]
+all_lines = LineReader.new('lib/sample_file.txt').complete
 
-# file.close
 
-#OR
-file_data = File.read("user.txt").split
-# ["user1", "user2", "user3"]
 
-File.foreach("users.txt") { |line| puts line }
+# # file = File.open("users.txt")
+# file_data = file.readlines.map(&:chomp)
+# # ["user1", "user2", "user3"]
+# # file.close
+#
+# #OR
+# file_data = File.read("user.txt").split
+# # ["user1", "user2", "user3"]
+#
+# File.foreach("users.txt") { |line| puts line }
