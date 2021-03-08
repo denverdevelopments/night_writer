5eab7e85b16c44821a2e2116fc4e7ac896bcaede
kevn7n <kevn_nguyen@yahoo.com>
2021-03-08 09:19:58 -0700

    Add user input cue to name file to translate in runner

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -2,13 +2,13 @@ require './lib/line_reader'
 require './lib/line'
 
 
-# print "Please enter the input file to convert to Braille:  "
-# read_from = gets.chomp
+print "Please enter the input file to convert to Braille:  "
+read_from = gets.chomp
 
-# all_lines = LineReader.new(read_from).complete
+all_lines = LineReader.new(read_from).complete
 # all_lines = LineGenerator.new('lib/messages.txt').complete
 
-all_lines = LineReader.new('lib/sample_file.txt').complete
+# all_lines = LineReader.new('lib/sample_file.txt').complete
 
 File.open("output.txt", "w") {|out| out.write(all_lines[0].text)}
 
@@ -17,11 +17,10 @@ p file.read
 file.close
 
 
-# # file = File.open("users.txt")
+
+
 # file_data = file.readlines.map(&:chomp)
 # # ["user1", "user2", "user3"]
-# # file.close
-#
 # #OR
 # file_data = File.read("user.txt").split
 # # ["user1", "user2", "user3"]
