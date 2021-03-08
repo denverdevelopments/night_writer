ce9d00bc30148e5a487bf1b7e4608293a2cfd657
kevn7n <kevn_nguyen@yahoo.com>
2021-03-08 10:35:24 -0700

    Add more line_reader tests for spaces, multi

--- test/line_reader_test.rb
+++ test/line_reader_test.rb
@@ -5,17 +5,40 @@ require './lib/line'
 
 class LineReaderTest < Minitest::Test
   def test_reader_exists
-    read_from = "lib/sample_file.txt"
+    read_from = "lib/sample_one.txt"
     reader = LineReader.new(read_from)
 
     assert_instance_of LineReader, reader
   end
 
-  def test_reader_has_lines
-    read_from = "lib/sample_file.txt"
+  def test_reader_reads_no_lines
+    read_from = "lib/sample_empty.txt"
+    reader = LineReader.new(read_from)
+
+    assert_equal "", reader.complete.first.text
+  end
+
+  def test_reader_reads_one_line
+    read_from = "lib/sample_one.txt"
     reader = LineReader.new(read_from)
 
     assert_equal "abc", reader.complete.first.text
   end
 
+  def test_reader_reads_multiple_lines
+    read_from = "lib/sample_two.txt"
+    reader = LineReader.new(read_from)
+
+    assert_equal "abc", reader.complete.first.text
+    assert_equal "de f g", reader.complete[1].text
+  end
+
+  def test_reader_reads_spaces
+    read_from = "lib/sample_two.txt"
+    reader = LineReader.new(read_from)
+
+    assert_equal "e", reader.complete[1].text[1]
+    assert_equal " ", reader.complete[1].text[2]
+    assert_equal " ", reader.complete[1].text[4]
+  end
 end
