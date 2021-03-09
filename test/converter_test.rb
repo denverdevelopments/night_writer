1b5158f80d36fa97f327dc8fdfd0488668f6d17e
kevn7n <kevn_nguyen@yahoo.com>
2021-03-09 07:59:42 -0700

    Try stubs to run methods

--- test/converter_test.rb
+++ test/converter_test.rb
@@ -3,6 +3,8 @@ require 'minitest/pride'
 require './lib/line_reader'
 require './lib/line'
 require './lib/converter'
+require 'mocha/minitest'
+
 
 class ConverterTest < Minitest::Test
   def test_it_exists
@@ -25,4 +27,23 @@ class ConverterTest < Minitest::Test
 
     assert_equal 246, converter.get_count?
   end
+
+  def test_it_gets_character_converts_to_braille
+    all_lines = LineReader.new("lib/row_braille.txt").complete
+    converter = Converter.new(all_lines)
+
+    converter.stubs(:to_braille).returns("0..0...0..00.0.0..0...0..00.0.")
+
+    assert_equal 30, converter.to_braille.length
+  end
+
+  def test_it_gets_character_converts_to_english
+    all_lines = LineReader.new("lib/row_braille.txt").complete
+    converter = Converter.new(all_lines)
+
+    converter.stubs(:to_english).returns("hi there")
+
+    assert_equal 8, converter.to_english.length
+  end
+
 end
