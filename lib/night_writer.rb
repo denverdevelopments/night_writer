d42b755bc69497631cde0807e5bc356ead7ef655
kevn7n <kevn_nguyen@yahoo.com>
2021-03-08 17:51:44 -0700

    Add ARGV to night_writer for 1 line terminal command

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,24 +1,33 @@
 require './lib/line_reader'
 require './lib/line'
+require './lib/converter'
 
 
-print "Please enter the input file to convert to Braille:  "
-read_from = "lib/#{gets.chomp}.txt"
+# print "Please enter the input file to convert to Braille:  "
+#
+# read_from = gets.chomp
+# # read_from = "lib/#{gets.chomp}.txt"
+#
+# print "Please enter the output file after the Braille conversion:  "
+#
+# write_to = gets.chomp
+# write_to = "#{gets.chomp}.txt"
 
-print "Please enter the output file after the Braille conversion:  "
-write_to = "#{gets.chomp}.txt"
+
+all_lines = LineReader.new(ARGV[0]).complete
+# all_lines = LineReader.new(read_from).complete
+
+converter = Converter.new(all_lines)
+converter.to_braille
 
 
-# read_from = 'lib/messages.txt'
-# read_from = 'lib/sample_two.txt'
+# write_to = "braille.txt"
 
-all_lines = LineReader.new(read_from).complete
-
-File.open(write_to, "w") do |out|
-  out.write(all_lines[0].text)
+File.open(ARGV[1], "w") do |out|
+  out.write(all_lines[0].text)    ##.join(" ")
 end
 
-file = File.open(write_to)
+file = File.open(ARGV[1])
 p file.read
 
 # File.open("output.txt", "w") {|out| out.write(all_lines[0].text)}
