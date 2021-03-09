e08f1251c452750daddc3896f55e6d671eb8167c
kevn7n <kevn_nguyen@yahoo.com>
2021-03-08 21:43:33 -0700

    Add more method tests to line_test & line_reader_test

--- test/line_reader_test.rb
+++ test/line_reader_test.rb
@@ -4,28 +4,28 @@ require './lib/line_reader'
 require './lib/line'
 
 class LineReaderTest < Minitest::Test
-  def test_reader_exists
+  def test_it_exists
     read_from = "lib/sample_one.txt"
     reader = LineReader.new(read_from)
 
     assert_instance_of LineReader, reader
   end
 
-  def test_reader_reads_no_lines
+  def test_it_reads_no_lines
     read_from = "lib/sample_empty.txt"
     reader = LineReader.new(read_from)
 
     assert_equal "", reader.complete.first.text
   end
 
-  def test_reader_reads_one_line
+  def test_it_reads_one_line
     read_from = "lib/sample_one.txt"
     reader = LineReader.new(read_from)
 
     assert_equal "abc", reader.complete.first.text
   end
 
-  def test_reader_reads_multiple_lines
+  def test_it_reads_multiple_lines
     read_from = "lib/sample_two.txt"
     reader = LineReader.new(read_from)
 
@@ -33,7 +33,7 @@ class LineReaderTest < Minitest::Test
     assert_equal "de f g", reader.complete[1].text
   end
 
-  def test_reader_reads_spaces
+  def test_it_reads_spaces
     read_from = "lib/sample_two.txt"
     reader = LineReader.new(read_from)
 
@@ -41,4 +41,32 @@ class LineReaderTest < Minitest::Test
     assert_equal " ", reader.complete[1].text[2]
     assert_equal " ", reader.complete[1].text[4]
   end
+
+  def test_it_reads_in_braille
+    read_from = "lib/row_braille.txt"
+    reader = LineReader.new(read_from)
+
+    assert_equal "0.0.0.0.0.", reader.complete[0].text
+    assert_equal "00.00.0..0", reader.complete[1].text
+    assert_equal "....0.0.0.", reader.complete[2].text
+  end
+
+  def test_it_reads_in_braille_80_dot_max
+    read_from = "lib/row_braille_82.txt"
+    reader = LineReader.new(read_from)
+
+    expected1 = "0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0."
+    expected2 = "................................................................................"
+
+    assert_equal 80, reader.complete[0].text.length
+    assert_equal expected1, reader.complete[0].text
+    assert_equal 80, reader.complete[1].text.length
+    assert_equal expected2, reader.complete[1].text
+    assert_equal 80, reader.complete[2].text.length
+    assert_equal expected2, reader.complete[2].text
+    assert_equal "0.", reader.complete[3].text
+    assert_equal "..", reader.complete[4].text
+    assert_equal "..", reader.complete[5].text
+  end
+
 end
--- test/line_test.rb
+++ test/line_test.rb
@@ -9,17 +9,63 @@ class LineTest < Minitest::Test
     assert_instance_of Line, line
   end
 
-  def test_has_empty_text_attribute
+  def test_has_attributes
     line = Line.new()
 
     assert_equal "", line.text
+    assert_equal [], line.top
+    assert_equal [], line.middle
+    assert_equal [], line.bottom
+    assert_equal [], line.all
   end
 
   def test_has_text_attribute
-    text = "abc"
-    line = Line.new(text)
+    line = Line.new("abc")
 
     assert_equal "abc", line.text
+  end
+
+  def test_scans_line
+    line = Line.new("abc")
+    line.scan_line
+
+    assert_equal ["O."], line.top
+    assert_equal [".."], line.middle
+    assert_equal [".."], line.bottom
+  end
+
+  def test_can_fill_top
+    line = Line.new("abc")
+    line.fill_top("a")
+
+    assert_equal ["O."], line.top
+  end
+
+  def test_can_fill_middle
+    line = Line.new("abc")
+    line.fill_middle("a")
+
+    assert_equal [".."], line.middle
+  end
+
+  def test_can_fill_bottom
+    line = Line.new("abc")
+    line.fill_bottom("a")
+
+    assert_equal [".."], line.bottom
+  end
+
+  def test_it_can_scan_row
+    line = Line.new("abc")
+    assert_equal [], line.all
+
+    line.scan_row
+
+    assert_equal "O.", line.all[0]
+    assert_equal "..", line.all[1]
+    assert_equal "..", line.all[2]
+      expected = ["O.", "..", ".."]
+    assert_equal expected, line.all
   end
 
 end
